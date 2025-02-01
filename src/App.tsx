4c34a463c0f35f577f3b2689037c6f7fa378fe5a
Yaser DEMET <ddemety372@gmail.com>
2025-02-01 09:12:45 +0300

    q0q1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+function App() {
+  return (
+    <>
+      <Button variant="contained">App</Button>
+    </>
+  );
+}
+
+export default App;
